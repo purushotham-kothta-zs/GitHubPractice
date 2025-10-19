a17c3e38ee9ccb08b7bf4b4900d19a588173740d
purushotham-kotha-zs <kotha.purushotham@zopsmart.com>
2025-10-19 13:33:21 +0530

    Wrote a basic program

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a =10;
+        fun();
+        int c = 30;
+    }
+
+    private static void fun(){
+        int a = 10;
+        int b = 20;
+        int c = 30;
+    }
+}
